9277cca7676bccbe23b65bbe07c0c304dfa941ce
Henry Crossley <crossh2001@yahoo.com>
2021-06-13 19:27:35 -0700

    add longblock image

--- index.js
+++ index.js
@@ -5,10 +5,17 @@ const body= document.body;
 const s1 = body.querySelector(".box_space .s1 img");
 const s2 = body.querySelector(".box_space .s2 img");
 const s3 = body.querySelector(".box_space .s3 img");
+const l1_1 = body.querySelector(".box_space .level1_1 img");
+const ul1_1 = body.querySelector(".box_space .unlevel1_1 img");
+const l1_2 = body.querySelector(".box_space .level1_2 img");
+const ul1_1 = body.querySelector(".box_space .unlevel1_2 img");
 
 /**change the element that has the question blocks to blank */
 s1.addEventListener("click", ()=>setTimeout(()=>s1.setAttribute('src', 'img/Question_Box_Off.gif'), 1), false);
 s2.addEventListener("click", ()=>setTimeout(()=>s2.setAttribute('src', 'img/Question_Box_Off.gif'), 1), false);
 s3.addEventListener("click", ()=>setTimeout(()=>s3.setAttribute('src', 'img/Question_Box_Off.gif'), 1), false);
+
+l1_1.addEventListener("click", ()=>ul1_1.style.visibility="visible", false);
+l1_2.addEventListener("click", ()=>ul1_2.style.visibility="visible", false);
 
 /**code to click on question blocks to change into blank blocks */
